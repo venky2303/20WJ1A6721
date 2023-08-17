f4b83d85b76b5cee574becfd684f49f090c69070
Venky <82228290+venky2303@users.noreply.github.com>
2023-08-17 12:53:43 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require('express');
+const axios = require('axios');
+const { performance } = require('perf_hooks');
+const app = express();
+const PORT = 3000;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+app.use(express.json());
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const VALID_URL_REGEX = /^(http|https):\/\/[^ "]+$/;
+const TIMEOUT_MS = 500;
+
+app.get('/numbers', async (req, res) => {
+  const urls = req.query.url;
+
+  if (!urls) {
+    return res.status(400).json({ error: 'At least one URL is required.' });
+  }
+
+  const validUrls = urls.filter(url => VALID_URL_REGEX.test(url));
+
+  const responses = [];
+
+  const startTime = performance.now();
+  const promises = validUrls.map(async (url, index) => {
+    try {
+      const response = await axios.get(url, { timeout: TIMEOUT_MS });
+      responses.push({
+        url,
+        number: index + 1,
+        data: response.data
+      });
+    } catch (error) {
+      responses.push({
+        url,
+        error: 'Request failed or timed out'
+      });
+    }
+  });
+
+  await Promise.all(promises);
+  const endTime = performance.now();
+
+  res.json({
+    processingTime: endTime - startTime,
+    responses: responses.sort((a, b) => a.number - b.number)
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
